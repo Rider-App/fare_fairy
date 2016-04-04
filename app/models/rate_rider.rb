a6bba834b6b949d31737131ccccfa8d82d0fb48e
Kate Studwell <katestudwell@gmail.com>
2016-04-04 17:40:21 -0400

    Create methods for rate rider model, create rate rider tests and fixtures.

---
+++ app/models/rate_rider.rb
@@ -0,0 +1,63 @@
+class RateRider
+  attr_reader :response, :start_lat, :start_lng, :end_lat, :end_lng, :origin, :destination
+
+  def initialize(origin, destination)
+    @origin_response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{origin}")
+    @origin = location_coordinates(@origin_response)
+    @destination_response = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{destination}")
+    @destination = location_coordinates(@destination_response)
+
+    @response = get_transit_data
+  end
+
+  def get_transit_data
+    options = []
+    options << Transit.new(@origin, @destination)
+  end
+
+  def formatted_addresses(response)
+    response["results"].map {|r| r["formatted_address"]}
+  end
+
+  def origin_address
+    formatted_addresses(@origin_response).first
+  end
+
+  def destination_address
+    formatted_addresses(@destination_response).first
+  end
+
+  def location_data(response)
+    response["results"].first["geometry"]["location"]
+  end
+
+  def lat(response)
+    location_data(response)["lat"]
+  end
+
+  def lng(response)
+    location_data(response)["lng"]
+  end
+
+  def start_lat
+    lat(@origin_response)
+  end
+
+  def start_lng
+    lng(@origin_response)
+  end
+
+  def end_lat
+    lat(@destination_response)
+  end
+
+  def end_lng
+    lng(@destination_response)
+  end
+
+  def location_coordinates(response)
+    "#{lat(response)}, #{lng(response)}"
+  end
+
+
+end
---
+++ test/models/rate_rider_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+require "#{Rails.root}/app/models/rate_rider.rb"
+
+class RateRider
+  def initialize
+    @origin_response = JSON.parse(File.read("#{Rails.root}/test/fixtures/rate_rider_test_origin.json"))
+    @destination_response = JSON.parse(File.read("#{Rails.root}/test/fixtures/rate_rider_test_destination.json"))
+
+    @origin = location_coordinates(@origin_response)
+    @destination = location_coordinates(@destination_response)
+
+  end
+end
+
+class RateRiderTest < ActiveSupport::TestCase
+
+  test "rate rider class exists" do
+    assert RateRider
+  end
+
+  test "rate rider returns a coordinate" do
+    rate_rider = RateRider.new
+    assert_equal 36.0182578, rate_rider.start_lat
+  end
+
+  test "can return an origin address" do
+    rate_rider = RateRider.new
+    assert_equal "1214 Broad St, Durham, NC 27705, USA", rate_rider.origin_address
+  end
+
+  test "can return a destination address" do
+    rate_rider = RateRider.new
+    assert_equal "514 W Pettigrew St, Durham, NC 27701, USA", rate_rider.destination_address
+  end
+
+end
