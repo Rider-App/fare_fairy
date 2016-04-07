4a9aeeef323128ddc15e905a163ba5b5582a89f0
Publicover <jameslpublicover@gmail.com>
2016-04-07 09:50:52 -0400

    delete migration for TaxiFare because it puts a bunch of weird stuff in bad places. recreate manually

--- db/migrate/20160406173409_create_taxi_fares.rb
+++
@@ -1,8 +0,0 @@
-class CreateTaxiFares < ActiveRecord::Migration
-  def change
-    create_table :taxi_fares do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- test/models/taxi_fare_test.rb
+++ test/models/taxi_fare_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
+require "#{Rails.root}/app/models/taxi_fare.rb"
 
-class TaxiFareTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class TaxiFare
+  def initialize
+
+    @nearest_city_response = JSON.parse(File.read("#{Rails.root}/test/fixtures/taxi_fare_test_nearest.json"))
+    @fare_response = JSON.parse(File.read("#{Rails.root}/test/fixtures/taxi_fare_test_fare.json"))
+    @companies_response = JSON.parse(File.read("#{Rails.root}/test/fixtures/taxi_fare_test_companies.json"))
+
+  end
+
+
+
+
+
+
+
+
+
+
 end
