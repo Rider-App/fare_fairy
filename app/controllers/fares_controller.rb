b4806322dcc7c7dd2f28cb0e2e41dbe49a5b491f
Kate Studwell <katestudwell@gmail.com>
2016-04-09 16:12:23 -0400

    Move taxi data to its own array in fares controller

--- app/controllers/fares_controller.rb
+++ app/controllers/fares_controller.rb
@@ -5,20 +5,20 @@ class FaresController < ApplicationController
     @rate_rider = RateRider.new(origin, destination)
     ride_sharing = []
     transit = []
+    taxi = []
 
     ride_sharing << Uber.new(@rate_rider.start_lat, @rate_rider.start_lng, @rate_rider.end_lat, @rate_rider.end_lng)
 
     ride_sharing << Lyft.new(@rate_rider.start_lat, @rate_rider.start_lng, @rate_rider.end_lat, @rate_rider.end_lng)
-
-    ride_sharing << TaxiFare.new(@rate_rider.start_lat, @rate_rider.start_lng, @rate_rider.end_lat, @rate_rider.end_lng)
 
     bus = GoogleTransit.new(origin, destination, "subway")
     if bus.valid?
       transit << bus
       subway = GoogleTransit.new(origin, destination, "bus")
       transit << subway unless subway.ride_name == bus.ride_name && subway.travel_type == bus.travel_type
-
     end
+
+    taxi << TaxiFare.new(@rate_rider.start_lat, @rate_rider.start_lng, @rate_rider.end_lat, @rate_rider.end_lng)
 
     @ride_sharing = ride_sharing
     @transit = transit
