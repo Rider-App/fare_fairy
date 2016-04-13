7bf2aae96bd27cd6a52d0181f93df8df40d1ee07
Kate Studwell <katestudwell@gmail.com>
2016-04-13 11:08:54 -0400

    Add task to destroy tokens older than a week

--- app/models/session_token.rb
+++ app/models/session_token.rb
@@ -1,7 +1,11 @@
 class SessionToken < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
-  validates :token, presence: true  
+  validates :token, presence: true
+
+  def self.cleanup_old_tokens
+    SessionToken.where("created_at < ?", 1.week.ago).destroy_all
+  end
 
 
 end
