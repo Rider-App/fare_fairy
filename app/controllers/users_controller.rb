20a6d66f354481c17e20391d72510f903edd4da5
Kate Studwell <katestudwell@gmail.com>
2016-04-13 21:45:46 -0400

    Change the render method for users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render :show, status: :created, location: @user
+      render 'show.json.jbuilder', status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
